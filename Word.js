297e7207a771ebe5f787a3a025e633ebf6d80aad
Duncan <duncan@Duncans-MacBook-Pro.local>
2018-07-22 22:09:39 -0500

    random word selected

--- Word.js
+++ Word.js
@@ -1,9 +1,9 @@
 var Letter = require("./Letter.js");
 var inquirer = require("inquirer");
 
-var array = ["dog", "cat", "mouse"];
+var array = ["dog", "cat", "mouse", "bear", "moose", "horse", "snail"];
+var guessedLetters = [];
 
- 
 
 function WordSelecter(word) {
     this.word = word;
@@ -15,14 +15,23 @@ function WordSelecter(word) {
 WordSelecter.prototype.splitFunction = function () {
     console.log(this.letters);
     //return this.letters.map(val => val.underscore()).join("");
-    var wordIsEqualto = this.letters.map(function(val){
+    var wordIsEqualto = this.letters.map(function (val) {
         return val.underscore();
     })
     console.log(wordIsEqualto.join(""));
 }
 
 
-var hangmanWord = new WordSelecter("dog")
+var randomNumber =  Math.floor(Math.random() * array.length)
+
+
+
+
+
+
+
+var hangmanWord = new WordSelecter(array[randomNumber]);
+
 
 
 
@@ -31,31 +40,40 @@ function playGame() {
 
 
 
-hangmanWord.splitFunction();
-
-    
-inquirer.prompt([
-    {
-        type: "input",
-        message: "Please guess a letter",
-        name: "answer"
-    }
-])
-.then(function(answer) {
+    hangmanWord.splitFunction();
 
 
-    var userGuess = answer.answer
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please guess a letter",
+            name: "answer"
+        }
+    ])
+        .then(function (answer) {
 
-hangmanWord.letters.forEach(val => {
-    
 
-    if (userGuess === val.letter) {
-        val.ifshowing = true
-        playGame();
-    }
-    
-})
-})
+            var userGuess = answer.answer
+
+            guessedLetters.push(userGuess);
+
+            console.log("You have guess these letters: " + guessedLetters);
+
+            hangmanWord.letters.forEach(val => {
+
+
+                if (userGuess === val.letter) {
+                    val.ifshowing = true
+                    playGame();
+                }
+
+                //    if (!userGuess === val.letter) {
+                //         console.log("That letter is not correct. Guess again.");
+                //         playGame();
+                //     }
+
+            })
+        })
 
 
 }

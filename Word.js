76201f049efa11abd9d98b7659682e029451e9ee
Duncan <duncan@Duncans-MacBook-Pro.local>
2018-07-23 10:06:18 -0500

    win feature working

--- Word.js
+++ Word.js
@@ -5,6 +5,7 @@ var array = ["dog", "cat", "mouse", "bear", "horse", "snail"];
 var guessedLetters = [];
 
 
+
 function WordSelecter(word) {
     this.word = word;
     this.letters = word.split("").map(val => new Letter(val, false))
@@ -12,8 +13,14 @@ function WordSelecter(word) {
 
 }
 
+console.log("=================================")
+console.log("Welcome to Animal Kingdom Hangman!")
+
+
+
 WordSelecter.prototype.splitFunction = function () {
     //console.log(this.letters);
+console.log(lettersLeft);
     //return this.letters.map(val => val.underscore()).join("");
     var wordIsEqualto = this.letters.map(function (val) {
         return val.underscore();
@@ -25,7 +32,7 @@ WordSelecter.prototype.splitFunction = function () {
 var randomNumber = Math.floor(Math.random() * array.length)
 var hangmanWord = new WordSelecter(array[randomNumber]);
 
-
+var lettersLeft = array[randomNumber].length;
 
 
 
@@ -50,27 +57,37 @@ function playGame() {
 
             guessedLetters.push(userGuess);
 
+            console.log("=================================");
             console.log("You have guess these letters: " + guessedLetters);
+
 
             hangmanWord.letters.forEach(val => {
 
 
                 if (userGuess === val.letter) {
                     val.ifshowing = true
-                   
-                }  
-
-                
-               
+                    lettersLeft--;
+                }
             });
 
-            playGame();
+            
+
+            if (lettersLeft === 0) {
+                console.log("You Win!")
+            
+
+            } else {
+                playGame();
+            }
+
         });
 
 
 };
 
+
 playGame();
+
 
 
 
